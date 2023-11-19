d2dd55518e50a04bc90fb36403250a3ee832d25d
RalucaElenaB <128471649+RalucaElenaB@users.noreply.github.com>
2023-11-19 18:21:28 +0200

    commit

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -3,7 +3,7 @@ import { InfinitySpin } from 'react-loader-spinner';
 const Loader = () => {
   return (
     <div className="Center">
-      <InfinitySpin width="250" color="#3f51b5" />
+      <InfinitySpin width="350" color="#3f51b5" />
     </div>
   );
 };
